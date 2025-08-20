c4cbe15268b7cca9382d1b2b087a5910d303ddcb
Tejesh Das <tejesh.das@incture.com>
2025-08-20 05:52:02 +0000

    Implemented ShellBar

--- customwebportal/webapp/controller/App.controller.js
+++ customwebportal/webapp/controller/App.controller.js
@@ -1,10 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/m/MessageToast"
+], function (Controller, Fragment, MessageToast) {
     "use strict";
 
     return Controller.extend("com.inctue.customwebportal.controller.App", {
-        onInit() {
-        }
+        
+        onInit: function () {
+            // Initialize any required data here
+        },
+
+        /**
+         * Event handler for avatar press - opens profile popover
+         */
+        onAvatarPress: function (oEvent) {
+            var oButton = oEvent.getSource(),
+                oView = this.getView();
+
+            // Create popover if it doesn't exist
+            if (!this._pPopover) {
+                this._pPopover = Fragment.load({
+                    id: oView.getId(),
+                    name: "com.inctue.customwebportal.fragments.UserProfilePopover",
+                    controller: this
+                }).then(function (oPopover) {
+                    oView.addDependent(oPopover);
+                    return oPopover;
+                });
+            }
+            
+            this._pPopover.then(function (oPopover) {
+                oPopover.openBy(oButton);
+            });
+        },
+
+        
     });
 });
